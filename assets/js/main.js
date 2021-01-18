ded183b5aba7329516567dadeb1cec06354ec0e5
elerel <elcranley@gmail.com>
2021-01-18 22:39:00 +0000

    added grid section to the DOM on main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,65 +1,138 @@
-const cards = document.querySelectorAll('.memory-card');
+// card data
 
-let hasFlippedCard = false;
-let lockBoard = false;
-let firstCard, secondCard;
+const cardsArray = [
 
-function flipCard() {
-  if (lockBoard) return;
-  if (this === firstCard) return;
+{
+    name: 'aircanada',
+    img: 'assets/img/ACA.png'
+},
+{
+    name: 'amu',
+    img: 'assets/img/AMU.png'
+},
+{
+    name: 'CHH2',
+    img: 'img/CHH2.png'
+},
+{
+    name: 'FIN',
+    img: 'assets/img/FIN2.png'
+},
+{
+    name: 'emirates',
+    img: 'assets/img/UAE.png'
+},
+{
+    name: 'Malay',
+    img: 'assets/img/MAS.png'
+},
+{
+    name: 'Ryanair',
+    img: 'assets/img/RYR.png'
+},
+{
+    name: 'Scandinavian',
+    img: 'assets/img/SAS.png'
+},
+{
+    name: 'swr',
+    img: 'assets/img/SWR.png'
+},
+{
+    name: 'thai',
+    img: 'assets/img/THA.png'
+},
+{
+    name: 'sli',
+    img: 'assets/img/SLI.png'
+},
+{
+    name: 'turk',
+    img: 'assets/img/THY.png'
+},
+];
 
-  this.classList.add('flip');
+var gameGrid = cardsArray.concat(cardsArray).sort(function () {
+  return 0.5 - Math.random();
+});
 
-  if (!hasFlippedCard) {
-    // first click
-    hasFlippedCard = true;
-    firstCard = this;
+var firstGuess = '';
+var secondGuess = '';
+var count = 0;
+var previousTarget = null;
+var delay = 1200;
 
+var game = document.getElementById('game');
+var grid = document.createElement('section');
+grid.setAttribute('class', 'grid');
+game.appendChild(grid);
+
+gameGrid.forEach(function (item) {
+  var name = item.name,
+      img = item.img;
+
+
+  var card = document.createElement('div');
+  card.classList.add('card');
+  card.dataset.name = name;
+
+  var front = document.createElement('div');
+  front.classList.add('front');
+
+  var back = document.createElement('div');
+  back.classList.add('back');
+  back.style.backgroundImage = 'url(' + img + ')';
+
+  grid.appendChild(card);
+  card.appendChild(front);
+  card.appendChild(back);
+});
+
+var match = function match() {
+  var selected = document.querySelectorAll('.selected');
+  selected.forEach(function (card) {
+    card.classList.add('match');
+  });
+};
+
+var resetGuesses = function resetGuesses() {
+  firstGuess = '';
+  secondGuess = '';
+  count = 0;
+  previousTarget = null;
+
+  var selected = document.querySelectorAll('.selected');
+  selected.forEach(function (card) {
+    card.classList.remove('selected');
+  });
+};
+
+grid.addEventListener('click', function (event) {
+
+  var clicked = event.target;
+
+  if (clicked.nodeName === 'SECTION' || clicked === previousTarget || clicked.parentNode.classList.contains('selected') || clicked.parentNode.classList.contains('match')) {
     return;
   }
 
-  // second click
-  secondCard = this;
+  if (count < 2) {
+    count++;
+    if (count === 1) {
+      firstGuess = clicked.parentNode.dataset.name;
+      console.log(firstGuess);
+      clicked.parentNode.classList.add('selected');
+    } else {
+      secondGuess = clicked.parentNode.dataset.name;
+      console.log(secondGuess);
+      clicked.parentNode.classList.add('selected');
+    }
 
-  checkForMatch();
-}
-
-function checkForMatch() {
-  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
-
-  isMatch ? disableCards() : unflipCards();
-}
-
-function disableCards() {
-  firstCard.removeEventListener('click', flipCard);
-  secondCard.removeEventListener('click', flipCard);
-
-  resetBoard();
-}
-
-function unflipCards() {
-  lockBoard = true;
-
-  setTimeout(() => {
-    firstCard.classList.remove('flip');
-    secondCard.classList.remove('flip');
-
-    resetBoard();
-  }, 1500);
-}
-
-function resetBoard() {
-  [hasFlippedCard, lockBoard] = [false, false];
-  [firstCard, secondCard] = [null, null];
-}
-
-// shuffle cards //
-
-(function shuffle() {
-  cards.forEach(card => {
-    let randomPos = Math.floor(Math.random() * 12);
-    card.style.order = randomPos;
-  });
-})();
-
-cards.forEach(card => card.addEventListener('click', flipCard));
+    if (firstGuess && secondGuess) {
+      if (firstGuess === secondGuess) {
+        setTimeout(match, delay);
+      }
+      setTimeout(resetGuesses, delay);
+    }
+    previousTarget = clicked;
+  }
+});
